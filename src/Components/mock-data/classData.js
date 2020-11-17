6c6512d3490470fdc8638f39d5af84e2e5589965
Matthew <mattk.development@gmail.com>
2020-11-17 16:26:47 -0600

    Added axiosWithAuth

--- src/Components/mock-data/classData.js
+++ src/Components/mock-data/classData.js
@@ -3,6 +3,8 @@
 let classesData = {
     1: {
         id: '1',
+        // time: '',
+        // duration: '',
         intensity: 'Intense',
         loc: 'Outside',
         NoU: '3',
@@ -10,6 +12,8 @@ let classesData = {
     },
     2: {       
         id: '2',
+        // time: '',
+        // duration: '',
         intensity: 'Light',
         loc: 'Living Room',
         NoU: '4',
@@ -17,9 +21,13 @@ let classesData = {
     },
     3: {       
         id: '3',
+        // time: '',
+        // duration: '',
         intensity: 'Medium',
         loc: 'Anywhere',
         NoU: '9',
         maxSize: '12',
     }
 }
+
+export default classesData
---
+++ src/Components/utils/axiosWithAuth.js
@@ -0,0 +1,13 @@
+// import axios from 'Axios';
+
+// export const axiosWithAuth = () => {
+
+//     const token = localStorage.getItem('token')
+
+//     return axios.create({
+//         baseURL: Url goes here,
+//         headers: {
+//             Authorization: `${token}`
+//         }
+//     })
+// }
