7034e1ae045b4bf2d3f87a7d93685987ecc20c12
Matthew <mattk.development@gmail.com>
2020-11-17 20:30:11 -0600

    Added Instructor page, fixed some routes

---
+++ src/Components/Employees/InstructrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Classes from './Classes.js'
+
+const InstructorClasses = () => {
+    return (
+        <div>
+            <h1>Create Classes</h1>
+            <Classes />
+        </div>
+    )
+}
+
+export default InstructorClasses
