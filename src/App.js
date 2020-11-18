f81e8a919fac15d6d0ffbc6c1de14fe77bee4f09
Matthew <mattk.development@gmail.com>
2020-11-17 20:55:36 -0600

    Refactored app.js

--- src/App.js
+++ src/App.js
@@ -7,37 +7,34 @@ import UserCreate from './Components/Clients/UserCreate';
 import Members from './Components/Clients/Members';
 import LoginInstructor from './Components/Employees/LoginInstructor';
 import CreateInstructor from './Components/Employees/CreateInstructor';
-// import Classes from './Components/Employees/Classes';
 import InstructorClasses from './Components/Employees/InstructrorPage.js';
 
 function App() {
 
-return (
-    <div className="App">
-        <Router>        
-            <Route exact path='/' component={MainContent}>
-                <MainContent />
-            </Route>
-
-            <Switch>
-                <Route exact path='/login' component={UserLogin}>
-                    <UserLogin />
+    return (
+        <div className="App">
+            <Router>        
+                <Route exact path='/' component={MainContent}>
+                    <MainContent />
                 </Route>
 
-                <Route exact path='/createaccount' component={UserCreate} />
-                <Route exact path='/instructorlogin' component={LoginInstructor} />
-                <Route exact path='/instructor' component={CreateInstructor} />
-                {/* <Route exact path='/classes' component={Classes} /> */}
-                {/* Main Pages after login */}
+                <Switch>
+                    <Route exact path='/login' component={UserLogin}>
+                        <UserLogin />
+                    </Route>
 
-                <Route exact path='/members' component={Members} />
-                <Route exact path='/instructorpage' component={InstructorClasses} />
-            </Switch>
-        </Router>
-    </div>
+                    <Route exact path='/createaccount' component={UserCreate} />
+                    <Route exact path='/instructorlogin' component={LoginInstructor} />
+                    <Route exact path='/instructor' component={CreateInstructor} />
+
+                    {/* Main Pages after login */}
+                    <Route exact path='/members' component={Members} />
+                    <Route exact path='/instructorpage' component={InstructorClasses} />
+                </Switch>
+            </Router>
+        </div>
 
     );
-
 }
 
 export default App
