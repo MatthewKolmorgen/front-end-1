1c9992d78e887a0706dfdacf84135b8bb770c7d8
Matthew <mattk.development@gmail.com>
2020-11-17 21:07:08 -0600

    Added notes app.js

--- src/App.js
+++ src/App.js
@@ -19,15 +19,20 @@ function App() {
                 </Route>
 
                 <Switch>
+                    {/* Login Pages */}
+
                     <Route exact path='/login' component={UserLogin}>
                         <UserLogin />
                     </Route>
+                    <Route exact path='/instructorlogin' component={LoginInstructor} />
+                    
+                    {/* Create Pages */}
 
                     <Route exact path='/createaccount' component={UserCreate} />
-                    <Route exact path='/instructorlogin' component={LoginInstructor} />
                     <Route exact path='/instructor' component={CreateInstructor} />
 
                     {/* Main Pages after login */}
+                    
                     <Route exact path='/members' component={Members} />
                     <Route exact path='/instructorpage' component={InstructorClasses} />
                 </Switch>
