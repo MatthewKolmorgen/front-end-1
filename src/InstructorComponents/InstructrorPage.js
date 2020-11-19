ef23851fecb3336573c806ca75d94d664bfc5c51
Matthew <mattk.development@gmail.com>
2020-11-19 12:34:51 -0600

    Sample Data working, needs styling

--- src/InstructorComponents/InstructrorPage.js
+++ src/InstructorComponents/InstructrorPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CreateClass from './Classes.js';
-import EditClasses from './EditClass.js';
+// import EditClasses from './EditClass.js';
+import SampleClasses from './classData.js'
 
 // Need backend to Edit/Delete tasks will create Mockdata in it's place.
 
@@ -9,6 +10,8 @@ const InstructorClasses = () => {
         <div>
             <h1>Create Classes</h1>
             <CreateClass />
+            <h3>Todays Classes</h3>
+            <SampleClasses />
             {/* <h2>Edit Classes</h2>
             <EditClasses /> */}
         </div>
